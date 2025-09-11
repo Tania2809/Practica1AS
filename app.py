6e537a694673124c2952f83b2388ff579dcb624f
rodriVG86 <rodrigo.vargas.guevara@gmail.com>
2025-09-11 17:55:54 -0500

    Actualiza la función 'guardarEvento' para incluir los parámetros 'idCategoria', 'idLugar' e 'idCliente' en la inserción de eventos y corrige un error en 'eventos.html'.

--- app.py
+++ app.py
@@ -93,19 +93,24 @@ def guardarEvento():
         descripcionEvento = data.get("descripcionEvento")
         fechainicio = data.get("fechainicio")
         fechaFin = data.get("fechaFin")
-        
+        idCategoria = data.get("idCategoria")
+        idLugar = data.get("idLugar")
+        idCliente = data.get("idCliente")
     else:
         descripcionUbicacion = request.form.get("descripcionUbicacion")
         descripcionEvento = request.form.get("descripcionEvento")
         fechainicio = request.form.get("fechainicio")
         fechaFin = request.form.get("fechaFin")
+        idCategoria = request.form.get("idCategoria")
+        idLugar = request.form.get("idLugar")
+        idCliente = request.form.get("idCliente")
 
     cursor = con.cursor(dictionary=True)
     sql = """
-    INSERT INTO eventos (descripcionUbicacion, descripcionEvento, fechainicio, fechaFin)
-    VALUES (%s, %s, %s, %s)
+    INSERT INTO eventos (descripcionUbicacion, descripcionEvento, fechainicio, fechaFin, idCategoria, idLugar, idCliente)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
     """
-    val = (descripcionUbicacion, descripcionEvento, fechainicio, fechaFin)
+    val = (descripcionUbicacion, descripcionEvento, fechainicio, fechaFin, idCategoria, idLugar, idCliente)
     cursor.execute(sql, val)
     con.commit()
     cursor.close()
