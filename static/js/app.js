d5591a12779681ce383bdbad2d9d56c9948a8a32
Leobardo Daniel Vargas <ldvr30345@gmail.com>
2025-09-11 20:40:57 -0500

    UPDATE LUGARES

--- static/js/app.js
+++ static/js/app.js
@@ -203,12 +203,12 @@ app.controller("lugaresCtrl", function($scope, $http) {
     $scope.lugares = []
 
     $scope.allData = function() {
-    $scope.get("/lugares/all").then(function(res) {
+    $http.get("/lugares/all").then(function(res) {
         $("#tablaLugares").html(res.data)
     })
 }
      //inizializa el template
-    $http.get("/").then(function(res) {
+    $http.get("/lugares").then(function(res) {
         $http.allData()
     })
      Pusher.logToConsole = true
