245fd1b6035e001fd6d95217cf2450d30c09c764
Tania2809 <angel24torrest@gmail.com>
2025-09-12 19:53:59 -0500

    c1

--- static/js/app.js
+++ static/js/app.js
@@ -84,16 +84,14 @@ app.controller("appCtrl", function($scope, $http) {})
 app.controller("eventosCtrl", function($scope, $http) {
 
     $scope.eventos = []
-    $scope.allData = function() {
+        // Cargar datos de eventos
+    $scope.cargarEventos = function() {
         $http.get("/eventos/all").then(function(res) {
-            $("#tablaEventos").html(res.data)
-        })
-    }
-
-    // Obtener lista de categorías
-    $http.get("/eventos").then(function(res) {
-        $scope.allData()
-    })
+            $scope.eventos = res.data;
+            // Si usas el método de insertar HTML
+            // $("#tablaEventos").html(res.data);
+        });
+    };
     Pusher.logToConsole = true
     var pusher = new Pusher("db840e3e13b1c007269e", {
         cluster: 'us2'
